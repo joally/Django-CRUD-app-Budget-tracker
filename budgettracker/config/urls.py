15b1437667e98fb1e0546f9fd1d3a79beac67cdf
joally <joally14@gmail.com>
2025-05-02 11:59:23 -0600

    Reinitialize project structure: replace old config with new clean setup

--- budgettracker/config/urls.py
+++ budgettracker/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for budgettracker project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
+    path('', include('main_app.urls')),
 ]
--- budgettracker/main_app/views.py
+++ budgettracker/main_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+# Define the home view function
 def home(request):
-    return HttpResponse('<h1>Hello</h1>')
+   return HttpResponse('<h1>Hello</h1>')
+
+def about(request):
+    contact_details = 'you can reach support at support@budgettracker.com'
+    return  render(request, 'about.html',{
+        'contact': contact_details
+        })
