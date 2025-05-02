a26bfc4f0afa31a72cce9b28986b9c3b7fae7130
joally <joally14@gmail.com>
2025-05-02 17:19:15 -0600

    updated ../Pipfile ../Pipfile.lock  config/settings.py

--- budgettracker/main_app/models.py
+++ budgettracker/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    income =models.IntegerField()
+    expense = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+    
